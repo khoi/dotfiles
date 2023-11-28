a682660fe756d854ae647264a923ea9a15b79753
khoi <git@khoi.io>
2023-11-28 23:51:19 +0700

    Update .config/sketchybar/plugins/vpn.sh

--- dot_config/sketchybar/plugins/executable_vpn.sh
+++ dot_config/sketchybar/plugins/executable_vpn.sh
@@ -2,11 +2,11 @@
 source "$CONFIG_DIR/colors.sh"
 
 if scutil --nc list | grep "^\*" | grep Connected >>/dev/null; then
-    ICON="􀞡"
-    ICON_COLOR=$RED
-else
     ICON="􀞙"
     ICON_COLOR=$GREEN
+else
+    ICON="􀞡"
+    ICON_COLOR=$RED
 fi
 
 sketchybar --set $NAME icon=$ICON icon.color=$ICON_COLOR
