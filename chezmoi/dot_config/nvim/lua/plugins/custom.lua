6694c0f95c5e80d28c6d7847f2301b759bcb8bc7
khoi <git@khoi.io>
2024-05-18 15:41:28 +0700

    toggle term

--- chezmoi/dot_config/nvim/lua/plugins/custom.lua
+++ chezmoi/dot_config/nvim/lua/plugins/custom.lua
@@ -14,6 +14,20 @@ return {
     },
   },
 
+  -- toggle term
+  {
+    "akinsho/nvim-toggleterm.lua",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<c-t>]],
+        start_in_insert = true,
+        persist_size = true,
+        direction = "horizontal",
+      })
+    end,
+  },
+
   {
     "alexghergh/nvim-tmux-navigation",
     config = function()
