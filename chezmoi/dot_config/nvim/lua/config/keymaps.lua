87f07c8b8c57d41652e802e1c5895909a39e08cf
khoi <git@khoi.io>
2024-05-19 12:09:57 +0700

    Backspace to CTRL-^

--- chezmoi/dot_config/nvim/lua/config/keymaps.lua
+++ chezmoi/dot_config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,9 @@ map("i", "jj", "<ESC>", { desc = "jj to quit insert mode" })
 
 map("n", "<leader>w\\", "<cmd>vsplit<cr>", { desc = "Split Window Right" })
 
+-- map backspace to CTRL-^
+map("n", "<BS>", "<C-^>", { desc = "Switch to previous buffer" })
+
 if os.getenv("TMUX") then
   vim.keymap.set("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>")
   vim.keymap.set("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>")
