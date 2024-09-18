7d4fe987808d49276e2f76e335dc5b6ea6561a80
khoi <git@khoi.io>
2024-09-18 15:32:16 +0700

    Enhance ZSH history sync script with line count output
    - Add line count display for 1Password and local history files
    - Include merged history line count after synchronization
    - Improve script readability and user feedback

--- chezmoi/dot_bin/executable_sync_zsh_history_to_1password.sh
+++ chezmoi/dot_bin/executable_sync_zsh_history_to_1password.sh
@@ -13,11 +13,15 @@ op document get .zsh_history --force -o $history1
 test ! -f $history1 && echo "File $history1 not found" && exit 1
 test ! -f $history2 && echo "File $history2 not found" && exit 1
 
+# print out number of lines
+echo "1Password: $(wc -l $history1)"
+echo "Local    : $(wc -l $history2)"
+
 cat $history1 $history2 | gawk -v date="WILL_NOT_APPEAR$(date +"%s")" '{if (sub(/\\$/,date)) printf "%s", $0; else print $0}' | LC_ALL=C sort -u | gawk -v date="WILL_NOT_APPEAR$(date +"%s")" '{gsub('date',"\\\n"); print $0}' > $merged
 
 echo "Replacing $merged to + $history2"
 cp -f $merged $history2
 
+echo "Merged   : $(wc -l $history2)"
 op document edit .zsh_history $history2
-
 echo "DONE"
