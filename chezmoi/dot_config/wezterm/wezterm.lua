f5d0eaf411446455a65cda8b90b830c7b66b5386
khoi <git@khoi.io>
2024-10-07 18:06:43 +0700

    Add WezTerm configuration
    - Create initial WezTerm config file
    - Set font to JetbrainsMono Nerd Font
    - Disable tab bar and set window decorations
    - Implement dynamic color scheme based on system appearance

---
+++ chezmoi/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+local mux = wezterm.mux
+
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "GruvboxDark"
+	else
+		return "GruvboxLight"
+	end
+end
+
+config.font = wezterm.font("JetbrainsMono Nerd Font")
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+return config
