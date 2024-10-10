43115df35cc4f6866a175cae91c2eaf427ebef6e
khoi <git@khoi.io>
2024-10-10 14:01:26 +0700

    Revert "Try out Zellij"
    This reverts commit b25494968f18a305a0d5f560c2bb16381684ffeb.

--- chezmoi/dot_config/nvim/lua/config/keymaps.lua
+++ chezmoi/dot_config/nvim/lua/config/keymaps.lua
@@ -4,3 +4,10 @@ map("n", "<BS>", "<C-^>", { desc = "Switch to previous buffer" })
 
 -- map ; to : in normal mode
 map("n", ";", ":", { noremap = true })
+
+if os.getenv("TMUX") then
+  vim.keymap.set("n", "<C-h>", "<cmd>NvimTmuxNavigateLeft<cr>")
+  vim.keymap.set("n", "<C-j>", "<cmd>NvimTmuxNavigateDown<cr>")
+  vim.keymap.set("n", "<C-k>", "<cmd>NvimTmuxNavigateUp<cr>")
+  vim.keymap.set("n", "<C-l>", "<cmd>NvimTmuxNavigateRight<cr>")
+end
