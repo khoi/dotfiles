0e32ccf7f22c5d20e5dcb0c8ee7d391df68f2712
khoi <git@khoi.io>
2024-10-11 11:49:11 +0700

    Add auto-dark-mode plugin for automatic theme switching

--- chezmoi/dot_config/nvim/lua/plugins/custom.lua
+++ chezmoi/dot_config/nvim/lua/plugins/custom.lua
@@ -9,6 +9,18 @@ return {
     end,
   },
   {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+      end,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox-material",
