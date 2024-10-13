12ebf4716379542a91e52c5add3055a140506c10
khoi <git@khoi.io>
2024-10-13 14:53:51 +0700

    Enhance Kobo Vietnamese font installation script
    - Add welcome message with Vietnamese flag emoji
    - Implement custom fonts installation
    - Add check for KOBOeReader mount before copying fonts
    - Copy fonts from iCloud Drive to KOBOeReader

--- chezmoi/dot_bin/executable_kobo_install_vietnamese_font.sh
+++ chezmoi/dot_bin/executable_kobo_install_vietnamese_font.sh
@@ -2,6 +2,8 @@
 
 set -Eeuo pipefail
 
+echo "🇻🇳 Install Vietnamese font for Kobo eReader"
+
 curl -o ~/Downloads/KoboRoot.tgz -L https://github.com/lelinhtinh/kobo-tieng-viet/releases/download/v0.5.0/KoboRoot.tgz
 cp ~/Downloads/KoboRoot.tgz /Volumes/KOBOeReader/.kobo
 
@@ -15,3 +17,12 @@ curl -o ~/Downloads/dicthtml-en-vi.zip -L https://github.com/lelinhtinh/kobo-tie
 curl -o ~/Downloads/dicthtml-vi-en.zip -L https://github.com/lelinhtinh/kobo-tieng-viet/releases/download/v0.4.0/dicthtml-vi-en.zip
 ~/Downloads/dictutil install ~/Downloads/dicthtml-vi-en.zip
 
+echo "Installing custom fonts"
+
+# check if KOBOeReader is mounted
+if [ ! -d /Volumes/KOBOeReader ]; then
+  echo "KOBOeReader is not mounted"
+  exit 1
+fi
+
+cp -R "/Users/khoi/Library/Mobile Documents/com~apple~CloudDocs/KoboFonts/" /Volumes/KOBOeReader/fonts
