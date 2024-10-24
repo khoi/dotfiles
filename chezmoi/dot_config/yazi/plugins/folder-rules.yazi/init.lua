13371fcb7dd5fd34e5016269b2b6453e488488ac
khoi <git@khoi.io>
2024-10-24 20:29:05 +0700

    Add folder-specific sorting rules for Yazi file manager
    Implement custom sorting behavior for Downloads folder:
    - Sort by modified date in reverse order
    - Disable directory-first sorting
    - Use alphabetical sorting with directories first for other folders

---
+++ chezmoi/dot_config/yazi/plugins/folder-rules.yazi/init.lua
@@ -0,0 +1,12 @@
+local function setup()
+	ps.sub("cd", function()
+		local cwd = cx.active.current.cwd
+		if cwd:ends_with("Downloads") then
+			ya.manager_emit("sort", { "modified", reverse = true, dir_first = false })
+		else
+			ya.manager_emit("sort", { "alphabetical", reverse = false, dir_first = true })
+		end
+	end)
+end
+
+return { setup = setup }
