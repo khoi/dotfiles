9a82245a34c3b52a67e82289b6b2f773b42b8270
khoi <git@khoi.io>
2024-11-19 10:23:43 +0700

    Customize wezterm terminal with new font and tab bar settings
    - Switch from JetbrainsMono to BerkeleyMono Nerd Font
    - Enable and customize tab bar with bottom placement
    - Add window frame and cursor style configurations
    - Set initial terminal width to 160 columns
    - Remove unused mux requirement

--- chezmoi/dot_config/wezterm/wezterm.lua
+++ chezmoi/dot_config/wezterm/wezterm.lua
@@ -1,6 +1,5 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local mux = wezterm.mux
 
 local function get_appearance()
 	if wezterm.gui then
@@ -17,10 +16,21 @@ local function scheme_for_appearance(appearance)
 	end
 end
 
-config.font = wezterm.font("JetbrainsMono Nerd Font")
-config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
+config.bold_brightens_ansi_colors = true
 config.color_scheme = scheme_for_appearance(get_appearance())
+config.enable_tab_bar = true
+config.font = wezterm.font("BerkeleyMono Nerd Font Mono")
+config.initial_cols = 160
+
+config.show_new_tab_button_in_tab_bar = false
+config.show_tab_index_in_tab_bar = false
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 24
+config.window_decorations = "RESIZE"
+config.window_frame = {
+	font_size = 14.0,
+}
 config.window_padding = {
 	left = 0,
 	right = 0,
@@ -28,5 +38,6 @@ config.window_padding = {
 	bottom = 0,
 }
 config.window_close_confirmation = "NeverPrompt"
+config.default_cursor_style = "SteadyBlock"
 
 return config
