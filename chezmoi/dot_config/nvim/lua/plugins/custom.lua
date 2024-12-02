6a5579c6cfa9019f729bc5a3c0df70ae04a8941f
khoi <git@khoi.io>
2024-12-02 10:32:40 +0700

    Add neo-tree plugin and update existing plugin configurations

--- chezmoi/dot_config/nvim/lua/plugins/custom.lua
+++ chezmoi/dot_config/nvim/lua/plugins/custom.lua
@@ -232,4 +232,15 @@ return {
       },
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+        visible = true,
+      },
+    },
+  },
 }
