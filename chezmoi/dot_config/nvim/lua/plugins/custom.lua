d28041cf0073786cc2512864013ecfe3d70f2791
khoi <git@khoi.io>
2024-12-14 01:43:00 +0300

    Enhance Editor UI - Remove markdown preview functionality - Customize lualine status line settings for improved readability - Configure avante.nvim and mini.files plugins for enhanced user experience

--- chezmoi/dot_config/nvim/lua/plugins/custom.lua
+++ chezmoi/dot_config/nvim/lua/plugins/custom.lua
@@ -75,14 +75,6 @@ return {
       },
     },
   },
-
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
   -- lualine
   {
     "nvim-lualine/lualine.nvim",
