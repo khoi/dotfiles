803a56c20d4e8777841692aee6c319d92692f8fc
khoi <git@khoi.io>
2024-12-16 08:00:15 +0700

    Add fish shell environment configurations and eza plugin
    - Set up essential environment variables for development tools
      - Configure nvim as default editor
      - Disable various Homebrew analytics and auto-updates
      - Set up restic backup configuration with 1Password integration
    - Add common aliases for frequently used commands (nvim, bat, open)
    - Install fish-eza plugin for enhanced directory listing capabilities

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -9,3 +9,19 @@ fzf_configure_bindings --directory=\cf
 set -g async_prompt_functions _pure_prompt_git
 
 source ~/.config/fish/private_variables.fish
+
+# Global
+set -gx EDITOR nvim
+set -gx HOMEBREW_NO_ANALYTICS 1
+set -gx HOMEBREW_NO_AUTO_UPDATE 1
+set -gx HOMEBREW_VERBOSE 1
+set -gx GHQ_ROOT ~/Developer/code
+set -gx TLDR_AUTO_UPDATE_DISABLED 1
+
+set -gx RESTIC_REPOSITORY "rest:http://restic:restic@vault.local:8769"
+set -gx RESTIC_PASSWORD_COMMAND "op read 'op://Personal/vault/restic encryption'"
+# Aliases
+alias o open
+alias v nvim
+alias vim nvim
+alias cat bat
