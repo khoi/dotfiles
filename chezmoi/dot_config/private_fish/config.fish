f1f16767d465f0552b21db1c419ab72ec492c2e3
khoi <git@khoi.io>
2024-12-16 16:58:31 +0700

    Configure fzf directory search in Fish shell
    - Add fzf directory search binding using Ctrl+F
    - Set up Ctrl+T binding for directory search using _fzf_search_directory
    - Improve config organization with section comments

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -4,9 +4,14 @@ end
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+fzf_configure_bindings --directory=\cf
+
 set -g async_prompt_functions _pure_prompt_git
 
 source ~/.config/fish/private_variables.fish
+
+# Key bindings
+bind \ct _fzf_search_directory
 
 # Global
 set -gx EDITOR nvim
@@ -18,6 +23,7 @@ set -gx TLDR_AUTO_UPDATE_DISABLED 1
 
 set -gx RESTIC_REPOSITORY "rest:http://restic:restic@vault.local:8769"
 set -gx RESTIC_PASSWORD_COMMAND "op read 'op://Personal/vault/restic encryption'"
+
 # Aliases
 alias o open
 alias v nvim
