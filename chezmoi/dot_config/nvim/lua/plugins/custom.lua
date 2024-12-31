2542d5526289b9a810aa52b8c474e033dcef591f
khoi <git@khoi.io>
2024-12-31 20:47:07 +0700

    Switch to oil.nvim for file exploration
    - Add oil.nvim with e key binding for file navigation
    - Configure oil.nvim to show hidden files
    - Disable neo-tree.nvim in favor of oil.nvim
    - Set oil.nvim as default file explorer

--- chezmoi/dot_config/nvim/lua/plugins/custom.lua
+++ chezmoi/dot_config/nvim/lua/plugins/custom.lua
@@ -148,4 +148,18 @@ return {
       },
     },
   },
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<leader>e", "<cmd>Oil<CR>", desc = "File Explorer" },
+    },
+    opts = {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+    },
+  },
 }
