634c291d4807beae7a276d5ca7366730ec28c10b
khoi <git@khoi.io>
2025-03-08 15:53:33 +0700

    Add OrbStack integration to fish shell

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -33,3 +33,7 @@ fish_add_path ~/.local/bin
 
 # Load stuff
 zoxide init fish | source
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
