dbf91cbf021d1fdb310dce9a44a35dc6a0aa045c
khoi <git@khoi.io>
2025-04-01 10:54:19 +0700

    Bind Ctrl+i to zoxide interactive mode in fish

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -36,6 +36,8 @@ fish_add_path ~/.local/bin
 # Load stuff
 zoxide init fish | source
 
+bind \ci zi
+
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
