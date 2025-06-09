a1d927a5243c1e4532b209aac57e768ba0c15005
khoi <git@khoi.io>
2025-06-09 10:44:26 +0100

    Zellij

--- chezmoi/dot_config/nvim/lua/plugins/custom.lua
+++ chezmoi/dot_config/nvim/lua/plugins/custom.lua
@@ -32,20 +32,17 @@ return {
     },
   },
   {
-    "numToStr/Navigator.nvim",
+    "https://git.sr.ht/~swaits/zellij-nav.nvim",
     lazy = true,
     event = "VeryLazy",
     keys = {
-      { "<C-h>", "<cmd>NavigatorLeft<cr>", { silent = true, desc = "navigate left" } },
-      { "<C-j>", "<cmd>NavigatorDown<cr>", { silent = true, desc = "navigate down" } },
-      { "<C-k>", "<cmd>NavigatorUp<cr>", { silent = true, desc = "navigate up" } },
-      { "<C-l>", "<cmd>NavigatorRight<cr>", { silent = true, desc = "navigate right" } },
+      { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
+      { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
+      { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
+      { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
     },
-    config = function()
-      require("Navigator").setup({})
-    end,
+    opts = {},
   },
-
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
