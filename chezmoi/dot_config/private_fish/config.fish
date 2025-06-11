51a8460be85afa21632ea8b5244854785f889c5d
khoi <git@khoi.io>
2025-06-11 10:03:11 +0100

    Add automatic Zellij session attachment for Ghostty terminal
    - Export ZELLIJ_CONFIG_DIR environment variable for manual launches
    - Auto-attach to user-named Zellij session when using Ghostty terminal
    - Only activate in interactive shells to avoid script interference
    - Remove unused Windsurf binary path configuration

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -36,9 +36,20 @@ fzf_configure_bindings --directory=\cp
 # Load stuff
 zoxide init fish | source
 
+# Check if we're in an interactive shell
+if status is-interactive
+
+    # At this point, specify the Zellij config dir, so we can launch it manually if we want to
+    export ZELLIJ_CONFIG_DIR=$HOME/.config/zellij
+
+    # Check if our Terminal emulator is Ghostty
+    if [ "$TERM" = xterm-ghostty ]
+        if not set -q ZELLIJ
+            zellij attach -c "$(whoami)"
+        end
+    end
+end
+
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-# Added by Windsurf
-fish_add_path /Users/khoi/.codeium/windsurf/bin
