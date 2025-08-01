f2bc2597f5636f39e679024c3a068be6ca18ed10
khoi <git@khoi.io>
2025-08-01 11:22:34 +0100

    Replace zellij-nav with Navigator.nvim
    - Switch window navigation from zellij-nav.nvim to Navigator.nvim
    - Configure Navigator with auto detection and zoom handling
    - Update multiple plugin versions in lazy-lock.json

--- chezmoi/dot_config/nvim/lua/plugins/custom.lua
+++ chezmoi/dot_config/nvim/lua/plugins/custom.lua
@@ -32,16 +32,22 @@ return {
     },
   },
   {
-    "https://git.sr.ht/~swaits/zellij-nav.nvim",
+    "numToStr/Navigator.nvim",
     lazy = true,
     event = "VeryLazy",
     keys = {
-      { "<c-h>", "<cmd>ZellijNavigateLeft<cr>", { silent = true, desc = "navigate left" } },
-      { "<c-j>", "<cmd>ZellijNavigateDown<cr>", { silent = true, desc = "navigate down" } },
-      { "<c-k>", "<cmd>ZellijNavigateUp<cr>", { silent = true, desc = "navigate up" } },
-      { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right" } },
+      { "<c-h>", "<cmd>NavigatorLeft<cr>", { silent = true, desc = "navigate left" } },
+      { "<c-j>", "<cmd>NavigatorDown<cr>", { silent = true, desc = "navigate down" } },
+      { "<c-k>", "<cmd>NavigatorUp<cr>", { silent = true, desc = "navigate up" } },
+      { "<c-l>", "<cmd>NavigatorRight<cr>", { silent = true, desc = "navigate right" } },
     },
-    opts = {},
+    config = function()
+      require("Navigator").setup({
+        auto_save = nil,
+        disable_on_zoom = false,
+        mux = "auto",
+      })
+    end,
   },
   -- add more treesitter parsers
   {
