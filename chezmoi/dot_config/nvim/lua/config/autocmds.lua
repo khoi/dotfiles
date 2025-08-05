15103ea656f947c4f95bd007e087313444c7bdac
khoi <git@khoi.io>
2025-08-05 14:23:30 +0100

    Disable concealing for markdown files in Neovim

--- chezmoi/dot_config/nvim/lua/config/autocmds.lua
+++ chezmoi/dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  group = augroup("md_conceal"),
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
