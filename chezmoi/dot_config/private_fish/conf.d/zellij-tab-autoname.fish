aa4154cd13f8d9e79061fd37be2a2c99bf68f6f4
khoi <git@khoi.io>
2025-08-30 08:47:52 +0700

    Simplify Zellij tab autoname post-execution behavior
    - Remove conditional checks for ZELLIJ environment variable
    - Always restore tab name to current directory after command execution
    - Eliminate special handling for "z" command in post-exec hook

--- chezmoi/dot_config/private_fish/conf.d/zellij-tab-autoname.fish
+++ chezmoi/dot_config/private_fish/conf.d/zellij-tab-autoname.fish
@@ -11,13 +11,7 @@ if status is-interactive
         end
 
         function zellij_tab_name_update_post --on-event fish_postexec
-            if set -q ZELLIJ
-                set -l cmd_line (string split " " -- $argv)
-                set -l process_name $cmd_line[1]
-                if test "$process_name" = "z"
-                    command nohup zellij action rename-tab (prompt_pwd) >/dev/null 2>&1
-                end
-            end
+            command nohup zellij action rename-tab (prompt_pwd) >/dev/null 2>&1
         end
     end
 end
