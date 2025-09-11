8613de8a793d4df88421ad026474d673ab1474a0
khoi <git@khoi.io>
2025-09-11 13:06:37 +0700

    Refactor SketchyBar colors with centralized Catppuccin theme system
    - Add comprehensive colors.sh with Catppuccin Mocha palette and semantic mappings
    - Replace hardcoded hex colors with theme variables across all plugins and configs
    - Enable easy theme switching through centralized color configuration
    - Update battery, Spotify, and main bar components to use semantic color names

--- chezmoi/dot_config/sketchybar/plugins-laptop/executable_battery.sh
+++ chezmoi/dot_config/sketchybar/plugins-laptop/executable_battery.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# Load color scheme
+source "$HOME/.config/sketchybar/colors.sh"
+
 # Battery is here bcause the ICON_COLOR doesn't play well with all background colors
 
 PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
@@ -12,29 +15,29 @@ fi
 case ${PERCENTAGE} in
 [8-9][0-9] | 100)
     ICON=""
-    ICON_COLOR=0xffa6da95
+    ICON_COLOR=$COLOR_BATTERY_FULL
     ;;
 7[0-9])
     ICON=""
-    ICON_COLOR=0xffeed49f
+    ICON_COLOR=$COLOR_BATTERY_HIGH
     ;;
 [4-6][0-9])
     ICON=""
-    ICON_COLOR=0xfff5a97f
+    ICON_COLOR=$COLOR_BATTERY_MEDIUM
     ;;
 [1-3][0-9])
     ICON=""
-    ICON_COLOR=0xffee99a0
+    ICON_COLOR=$COLOR_BATTERY_LOW
     ;;
 [0-9])
     ICON=""
-    ICON_COLOR=0xffed8796
+    ICON_COLOR=$COLOR_BATTERY_CRITICAL
     ;;
 esac
 
 if [[ $CHARGING != "" ]]; then
     ICON=""
-    ICON_COLOR=0xffeed49f
+    ICON_COLOR=$COLOR_BATTERY_HIGH
 fi
 
 sketchybar --set $NAME \
