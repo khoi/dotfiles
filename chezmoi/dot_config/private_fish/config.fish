998690556d591f82552893fcd7a39b00e4888045
khoi <git@khoi.io>
2026-01-15 08:05:29 +0700

    remove fzf catppuccin theme colors

--- chezmoi/dot_config/private_fish/config.fish
+++ chezmoi/dot_config/private_fish/config.fish
@@ -28,13 +28,6 @@ set -gx GHQ_ROOT ~/Developer/code
 set -gx TLDR_AUTO_UPDATE_DISABLED 1
 set -gx XDG_CONFIG_HOME ~/.config
 
-# FZF - Catppuccin Macchiato theme
-set -gx FZF_DEFAULT_OPTS "\
---color=bg+:#363A4F,spinner:#F4DBD6,hl:#ED8796 \
---color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
---color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
---color=selected-bg:#494D64 \
---color=border:#6E738D,label:#CAD3F5"
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude .git --strip-cwd-prefix'
 set -gx FZF_CTRL_T_COMMAND 'fd --type f --hidden --exclude .git --strip-cwd-prefix'
 
