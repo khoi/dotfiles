b7bf6806dea5331121cdb9a625cdda0ffdbb3a81
khoi <6994441+khoi@users.noreply.github.com>
2026-03-06 03:29:12 +0000

    nvim: show hidden files in snacks picker

--- chezmoi/dot_config/nvim/lua/plugins/custom.lua
+++ chezmoi/dot_config/nvim/lua/plugins/custom.lua
@@ -57,6 +57,18 @@ table.insert(plugins, {
 
 local rest = {
   {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.picker = opts.picker or {}
+      opts.picker.sources = opts.picker.sources or {}
+      opts.picker.sources.files = vim.tbl_deep_extend("force", opts.picker.sources.files or {}, {
+        hidden = true,
+      })
+      return opts
+    end,
+  },
+  {
     "mrjones2014/smart-splits.nvim",
     lazy = false,
     keys = function()
